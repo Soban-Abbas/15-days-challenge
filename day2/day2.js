63263fe3f4cfbab06d8cfce74bf3c62558430c32
Soban Abbas <sobanabbass@gmail.com>
2025-08-22 22:17:39 +0500

    quiz app

--- day2/day2.js
+++ day2/day2.js
@@ -1,15 +1,32 @@
-let count ;
-function decrease(count){
+// let count ;
+// function decrease(count){
 
-   if(count<=0){
-     return count =0;
-   }else{
-    return count --;
-   }
+//    if(count<=0){
+//      return count =0;
+//    }else{
+//     return count --;
+//    }
+// }
+// const increase= function(count){
+//      return count++
+// }
+// const reset=(count)=>{
+//     return count =0;
+// }
+let score=0;
+let questions=["what is capital of pakistan ?", "president of pakistan?", "PM of pakistan"];
+let totalquestions=3;
+let answers=["ISB", "Zirdari", "Shobaz Shareef"];
+for (let i=0;i<totalquestions;i++){
+  let useranswer=prompt(questions[i]);
+  useranswer=String(useranswer);
+  if(useranswer===answers[i]){
+    console.log("correct");
+    score++;
+  }else{
+    console.log("wrong answer");
+  }
+
 }
-const increase= function(count){
-     return count++
-}
-const reset=(count)=>{
-    return count =0;
-}
+console.log("quiz finished ");
+console.log("your score in quiz are" + score );
