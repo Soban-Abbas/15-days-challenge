54a94a4d8610cca0dfd43eb43475a4bd1eda67b0
Soban Abbas <sobanabbass@gmail.com>
2025-08-27 20:16:10 +0500

    changing bg with options

---
+++ day6/DOM_Project.js
@@ -0,0 +1,40 @@
+let counter=document.getElementById("count")
+let heading=document.getElementById("h");
+let btn=document.getElementsByClassName("btn");
+counter.addEventListener("click",function(e){
+if(e.target.tagName==="BUTTON"){
+    e.stopPropagation();
+    
+    if(e.target.innerText==="Increase"){
+       Number(heading.innerText++);
+    }else if(e.target.innerText==="Reset" ){
+        heading.innerText=0;
+    }
+}else
+    return false;
+});
+
+//Change Background with dropdown
+let clrs=document.getElementsByClassName("clr");
+let select=document.getElementById("select").addEventListener("change",function(e){
+    // if(e.target.tagName==="OPTION"){
+    //     e.stopPropagation();
+    //     if(e.target.innerText==="Black"){
+    //         e.target.classList.toggle("Black");
+    //     }else if(e.target.innerText==="Pink"){
+    //         e.target.classList.toggle("Pink")
+    //     }else if(e.target.innerText==="Yellow"){
+    //         e.target.classList.toggle("Yellow")
+    //     }
+    // }else 
+    //     return false;
+    if(e.target.value==="Black"){
+          document.body.classList.toggle("Black");
+    }else if(e.target.value==="Pink"){
+           document.body.classList.toggle("Pink")
+    }else if(e.target.value==="Yellow"){
+        document.body.classList.toggle("Yellow")
+    }else {
+        return false;
+    }
+})
