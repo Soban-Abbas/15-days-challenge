c137c90c9191f32099d419606506ce5938e64508
Soban Abbas <sobanabbass@gmail.com>
2025-08-28 17:39:24 +0500

    Todo logic with DOM

--- day7/project.js
+++ day7/project.js
@@ -1,30 +1,30 @@
-let value=document.getElementById("value");
-let main=document.getElementById("div1").addEventListener("click",function(e){
-    if(e.target.tagName==="BUTTON"){
+// let value=document.getElementById("value");
+// let main=document.getElementById("div1").addEventListener("click",function(e){
+//     if(e.target.tagName==="BUTTON"){
 
 
-if(e.target.innerText==="Decrease"){
-    if(value.innerText>1){
-        value.innerText=Number(value.innerText)-1;
-    }else{
-        (value.innerText)=Number(0);
-    }
-}
+// if(e.target.innerText==="Decrease"){
+//     if(value.innerText>1){
+//         value.innerText=Number(value.innerText)-1;
+//     }else{
+//         (value.innerText)=Number(0);
+//     }
+// }
 
 
-else if(e.target.innerText==="Reset"){
-    value.innerText=Number(0);
-}
+// else if(e.target.innerText==="Reset"){
+//     value.innerText=Number(0);
+// }
 
 
-else if(e.target.innerText==="Increase"){
-      if(Number(value.innerText)===10){
-        alert("MAX Value Reached");
-    }else
-    value.innerText=Number(value.innerText)+1;
+// else if(e.target.innerText==="Increase"){
+//       if(Number(value.innerText)===10){
+//         alert("MAX Value Reached");
+//     }else
+//     value.innerText=Number(value.innerText)+1;
   
     
-}
+// }
 
 
 
@@ -37,23 +37,51 @@ else if(e.target.innerText==="Increase"){
 
 
 
-    }
-})
-//Multiplication Table Generator
-  let inputValue=document.getElementById("number");
-   let message1=document.getElementById("message");
-let button=document.getElementById("btn1");
-button.addEventListener("click",function(e){
+//     }
+// })
+// //Multiplication Table Generator
+//   let inputValue=document.getElementById("number");
+//    let message1=document.getElementById("message");
+// let button=document.getElementById("btn1");
+// button.addEventListener("click",function(e){
  
-    if(inputValue.value==="" || inputValue.value==="0" || Number(inputValue.value)>10){
-       message1.innerText="Please Enter correct b/w 0 and 11 ";
-    }else if(Number(inputValue.value)>0 || Number(inputValue.value)<=10){
+//     if(inputValue.value==="" || inputValue.value==="0" || Number(inputValue.value)>10){
+//        message1.innerText="Please Enter correct b/w 0 and 11 ";
+//     }else if(Number(inputValue.value)>0 || Number(inputValue.value)<=10){
 
-        for(let i=1;i<=10;i++){
+//         for(let i=1;i<=10;i++){
            
-           message1.innerHTML+=`<br>${inputValue.value} * ${i}=${inputValue.value*i}<br>`
-        }
+//            message1.innerHTML+=`<br>${inputValue.value} * ${i}=${inputValue.value*i}<br>`
+//         }
 
-    }
+//     }
 
+// })
+
+//grocery list
+let mainlist=document.getElementById("mainList");
+let task=document.getElementById("task");
+let message=document.getElementById("errormessage");
+let btn=document.getElementById("btn");
+btn.addEventListener("click",function(e){
+     if(task.value===""){
+        message.innerHTML=`<br> please add task`;
+     }else {
+        message.innerText="";
+        task.value.trim();
+        let newItem=document.createElement("li");
+        let deletebtn=document.createElement("button");
+        deletebtn.innerText="Delete";
+        
+        deletebtn.addEventListener("click",function(e){
+            
+           mainlist.removeChild(e.target.parent)
+
+        })
+        newItem.innerText=task.value;
+        mainlist.appendChild(newItem);
+       newItem.appendChild(deletebtn);
+        task.value="";
+
+     }
 })
