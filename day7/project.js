f896b2352b03f5f9ba108a4fdbbb9aad0bd713a7
Soban Abbas <sobanabbass@gmail.com>
2025-08-28 23:56:10 +0500

    3rd mini project

--- day7/project.js
+++ day7/project.js
@@ -59,29 +59,131 @@
 // })
 
 //grocery list
-let mainlist=document.getElementById("mainList");
-let task=document.getElementById("task");
-let message=document.getElementById("errormessage");
-let btn=document.getElementById("btn");
-btn.addEventListener("click",function(e){
-     if(task.value===""){
-        message.innerHTML=`<br> please add task`;
-     }else {
-        message.innerText="";
-        task.value.trim();
-        let newItem=document.createElement("li");
-        let deletebtn=document.createElement("button");
-        deletebtn.innerText="Delete";
+// let mainlist=document.getElementById("mainList");
+// let task=document.getElementById("task");
+// let message=document.getElementById("errormessage");
+// let btn=document.getElementById("btn");
+// btn.addEventListener("click",function(e){
+//      if(task.value===""){
+//         message.innerHTML=`<br> please add task`;
+//      }else {
+//         message.innerText="";
+//         task.value.trim();
+//         let newItem=document.createElement("li");
+//         let deletebtn=document.createElement("button");
+//         deletebtn.innerText="Delete";
         
-        deletebtn.addEventListener("click",function(e){
+//         deletebtn.addEventListener("click",function(e){
             
-           mainlist.removeChild(e.target.parent)
+//            mainlist.removeChild(e.target.parent)
 
-        })
-        newItem.innerText=task.value;
-        mainlist.appendChild(newItem);
-       newItem.appendChild(deletebtn);
-        task.value="";
+//         })
+//         newItem.innerText=task.value;
+//         mainlist.appendChild(newItem);
+//        newItem.appendChild(deletebtn);
+//         task.value="";
 
-     }
+//      }
+// })
+
+
+let mainArray=[];
+
+let inputValue=document.getElementById("task");
+
+let addButton=document.getElementById("btn");
+
+
+let message=document.querySelector("span");
+
+let mainList=document.getElementById("main");
+
+
+addButton.addEventListener("click",function(e){
+
+if(inputValue.value===""){
+    message.innerHTML=`<br> Enter Task`;
+
+}else{
+
+let task={
+    id: Date.now(),
+    texk: inputValue.value
+};
+mainArray.push(task);
+
+let list=document.createElement("li");
+list.innerText=task.texk;
+
+list.dataset.id=task.id;
+
+mainList.appendChild(list)
+console.log(mainArray);
+let deleteBtn=document.createElement("button");
+deleteBtn.innerText="Delete";
+list.appendChild(deleteBtn);
+deleteBtn.addEventListener("click",function(e){
+    let id=Number(list.dataset.id);
+    let indx=mainArray.findIndex(function(t){
+        t.id===id;
+    })
+    if(indx>-1){
+        mainArray.splice(indx,1)
+    }
+    list.remove();
+})
+message.innerText="";
+inputValue.value="";
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
