392167ca9a78b8dfd9e794c2a0be92f963765f22
Soban Abbas <sobanabbass@gmail.com>
2025-09-02 20:26:50 +0500

    storing from details in local store and retrieving on DOM

--- day11/day11.js
+++ day11/day11.js
@@ -1,19 +1,37 @@
-let usernumber=prompt("Enter Number");
-let count=document.getElementById("count");
-let number = Number(usernumber);
-let id=setInterval(function(
-){
+let myForm=document.getElementById("myform");
+let details=document.getElementById("details");
+myForm.addEventListener("submit",function(e){
+  e.preventDefault();
 
-  
-  count.innerText=number;
-  number--;
-  if(number===0){
-     count.innerText="";
-    clearInterval(id);
-   
-  }
+  let name=document.getElementById("name").value ;
+  let email=document.getElementById("email").value;
 
 
+let detObj={
+  id:Date.now(),
+  Name:name,
+  Email:email
+}
 
 
-},1000)
+
+
+let makeString=JSON.stringify(detObj);
+localStorage.setItem("details",makeString);
+details.innerText=`Name: ${detObj.Name} Email: ${detObj.Email}`;
+
+
+myForm.reset();
+
+})
+
+
+
+let retstring =localStorage.getItem("details");
+let retobj=JSON.parse(retstring);
+
+
+details.innerText=`Name: ${retobj.Name} Email: ${retobj.Email}`;
+
+
+
