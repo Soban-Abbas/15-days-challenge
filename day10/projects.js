104a741e4e3b5d42b8c4cb30a3d662bf197d5d4e
Soban Abbas <sobanabbass@gmail.com>
2025-09-02 11:03:29 +0500

    live search

--- day10/projects.js
+++ day10/projects.js
@@ -1,16 +1,50 @@
-let darkLightbtn=document.getElementById("darklightmode");
-darkLightbtn.addEventListener("click",function(e){
-    document.body.classList.toggle("dark");
-    if(document.body.classList.contains("dark")){
-        localStorage.setItem("theme","dark")
-    }else{
-        localStorage.setItem("theme","light")
-    }
-})
+let search=document.getElementById("search");
+let list=document.getElementsByTagName("li");
+let message=document.getElementById("notFound");
+search.addEventListener("input",function(e){
+  let found=false;
+  let userInput=search.value.toLowerCase();
+
+  for(let i=0;i<list.length;i++)
+{
+  let listContent=list[i].innerText.toLowerCase();
+  if(listContent.includes(userInput)){
+    list[i].style.display="";
+    found =true;
+
+  }else{
+list[i].style.display="none";
+  }
+  if(!found){
+    message.style.display="";
+  }else{
+    message.style.display="none";
+  }
 
 
-if(localStorage.getItem("theme")==="dark"){
-    document.body.classList.add("dark");
-}else if (localStorage.getItem("theme")==="light"){
-    document.body.classList.add("light");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+})
