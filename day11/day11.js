d3a1b84f61a5585f88fcd0e04f6f6c1df82a7d0f
Soban Abbas <sobanabbass@gmail.com>
2025-09-02 18:19:30 +0500

    showing heading word after word

--- day11/day11.js
+++ day11/day11.js
@@ -1,41 +1,16 @@
-let retString = localStorage.getItem("notes");
-let notesArray = retString ? JSON.parse(retString) : [];
-let ulList = document.getElementById("notesList");
-let inputNotes = document.getElementById("notes");
-let addBtn = document.getElementById("btn");
-let errorMessage = document.getElementById("message");
+let sentenceArray = ["I", "am", "learning", "JavaScript", "every", "day"];
 
-addBtn.addEventListener("click", function (e) {
-  if (inputNotes.value === "") {
-    errorMessage.innerHTML = "<br>Enter Note";
-  } else {
-    let noteObj = {
-      id: Date.now(),
-      noteContent: inputNotes.value,
-    };
-    notesArray.push(noteObj);
-    let listContent = document.createElement("li");
-    listContent.dataset.id = noteObj.id;
-    listContent.innerText = noteObj.noteContent;
-    ulList.appendChild(listContent);
 
-    let makeString = JSON.stringify(notesArray);
-    localStorage.setItem("notes", makeString);
-  }
-  inputNotes.value = "";
-});
+let heading=document.querySelector("h2");
 
-let retArray = JSON.parse(retString);
+let i=0;
+let id=setInterval(function(){
 
-for (let i = 0; i < retArray.length; i++) {
-  let listContent = document.createElement("li");
-  listContent.dataset.id = retArray[i].id;
-  listContent.innerText = retArray[i].noteContent;
-  ulList.appendChild(listContent);
+heading.innerText=`${heading.innerText} ${sentenceArray[i]}`;
+i++;
+if(i>=sentenceArray.length){
+  clearInterval(id);
 }
 
-inputNotes.addEventListener("input", function (e) {
-  if (inputNotes.value !== "") {
-    errorMessage.innerText = "";
-  }
-});
+
+},1000)
