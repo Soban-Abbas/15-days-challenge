c1866d013ad65d68367c494554542b4682044737
Soban Abbas <sobanabbass@gmail.com>
2025-09-04 23:17:48 +0500

    adding readme file

--- day13/day13.js
+++ day13/day13.js
@@ -1,43 +1,54 @@
 let inputNotes = document.getElementById("notes");
 let uL = document.getElementById("noteList");
-
 let saveArray = JSON.parse(localStorage.getItem("notes") || "[]");
-let addBtn = document.getElementById("addNotes");
-addBtn.addEventListener("click", function (e) {
-  if (inputNotes.value === "") {
-    document.getElementById("message").innerHTML = "<br>Enter Notes";
-  } else {
-    let noteObj = {
-      id: Date.now(),
-      note_Content: inputNotes.value,
-    };
 
-    saveArray.push(noteObj);
-
-    inputNotes.value = "";
-
+function renderNotes() {
+  uL.innerHTML = "";
+  saveArray.forEach((noteObj) => {
     let list = document.createElement("li");
-    uL.appendChild(list);
-
     list.dataset.id = noteObj.id;
     list.innerText = noteObj.note_Content;
 
-    localStorage.setItem("notes", JSON.stringify(saveArray));
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    list.appendChild(deleteBtn);
+
+    uL.appendChild(list);
+  });
+}
+
+function addNote() {
+  if (inputNotes.value === "") {
+    document.getElementById("message").innerHTML = "<br>Enter Notes";
+    return;
+  }
+
+  let noteObj = {
+    id: Date.now(),
+    note_Content: inputNotes.value,
+  };
+
+  saveArray.push(noteObj);
+  localStorage.setItem("notes", JSON.stringify(saveArray));
+  inputNotes.value = "";
+  renderNotes();
+}
+
+function deleteNote(id) {
+  saveArray = saveArray.filter((obj) => obj.id !== id);
+  localStorage.setItem("notes", JSON.stringify(saveArray));
+  renderNotes();
+}
+
+// Event Listeners
+document.getElementById("addNotes").addEventListener("click", addNote);
+
+uL.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON") {
+    let li = e.target.parentElement;
+    deleteNote(Number(li.dataset.id));
   }
 });
 
-(function () {
-  let output = localStorage.getItem("notes");
-
-  if (output !== null) {
-    let newarray = saveArray.map(function (t) {
-      return t.note_Content;
-    });
-
-    for (let item of newarray) {
-      let list = document.createElement("li");
-      list.innerText = item;
-      uL.appendChild(list);
-    }
-  }
-})();
+// Initial render
+renderNotes();
---
+++ day13/readme.md
@@ -0,0 +1,7 @@
+# day#13 4 sep,2025
+## Topic cover
+- Immediatly invocked function expression
+- Recursion
+## Try a project 
+- but fail to complete 
+- notes to store in local storage and update dynamicallty from local storage using concept of   event deligation
